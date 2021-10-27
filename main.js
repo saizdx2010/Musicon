2ee7ccc753f857131a85a29ac603a3135adcc216
saizdx2010 <saiprashanth02012001@gmail.com>
2021-10-27 15:15:37 +0700

    finished templating index.html and store.html

--- main.js
+++ main.js
@@ -30,3 +30,14 @@ const context = {
         }
     ]
 };
+
+
+const templateElement = document.getElementById("templateHB");
+
+const templateSource = templateElement.innerHTML;
+
+const template = Handlebars.compile(templateSource);
+
+const compiledHtml = template(context);
+
+document.getElementById('information').innerHTML = compiledHtml;
